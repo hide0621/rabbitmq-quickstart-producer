3c945b0b798455fa1d7db870f84cbdd089efef1a
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-10-21 11:36:07 +0900

    producerが見積もりを受け取れるようにした

--- src/main/kotlin/org/acme/rabbitmq/producer/QuotesResource.kt
+++ src/main/kotlin/org/acme/rabbitmq/producer/QuotesResource.kt
@@ -1,9 +1,12 @@
 package org.acme.rabbitmq.producer
 
+import io.smallrye.mutiny.Multi
+import jakarta.ws.rs.GET
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import org.acme.rabbitmq.model.Quote
 import org.eclipse.microprofile.reactive.messaging.Channel
 import org.eclipse.microprofile.reactive.messaging.Emitter
 
@@ -15,6 +18,9 @@ class QuotesResource {
     @Channel("quote-requests")
     lateinit var quoteRequestEmitter: Emitter<String>
 
+    @Channel("quotes")
+    lateinit var quotes: Multi<Quote>
+
     @POST
     @Path("/request")
     @Produces(MediaType.TEXT_PLAIN)
@@ -22,5 +28,11 @@ class QuotesResource {
         val uuid = UUID.randomUUID()
         quoteRequestEmitter.send(uuid.toString())
         return uuid.toString()
+    }
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    fun stream(): Multi<Quote> {
+        return quotes
     }
 }
